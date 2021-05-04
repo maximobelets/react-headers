ef79af8960b035a71e234f4b3bd65331847e8ac9
Maxim Obelets <maximobelets@gmail.com>
2021-05-05 00:41:51 +0500

    Add new logic for dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -9,16 +9,32 @@ interface IProps {
 export const Dropdown = ({list}: IProps) => {
 	const [isActive, setActive] = useState(false);
 	const [placeholder, setPlaceholder] = useState('Dropdown');
+	const [inputValue, setInputValue] = useState('');
+
+	const handleInput = (query: any) => {
+		setInputValue(query);
+	};
+
+	const elementListClick = (data: any) => {
+		setPlaceholder(data);
+		setInputValue(data);
+	}
+
+	console.log(inputValue)
 
 	return (
 		<>
-			<div className={s.root} onClick={() => setActive(!isActive)}>
-				{placeholder}
-			</div>
+			<input 
+				className={s.root}
+				value={inputValue}
+				onClick={() => setActive(!isActive)}
+				onChange={(event) => handleInput(event.currentTarget.value)}
+				placeholder={placeholder}
+			/>
 			{isActive ? (
 				<ul className={s.list}>
 					{list.map((el: TestArray) => 
-						<li className={s.el} onClick={() => setPlaceholder(el.name)}>
+						<li className={s.el} onClick={() => elementListClick(el.name)}>
 							{el.name}
 						</li>
 					)}
