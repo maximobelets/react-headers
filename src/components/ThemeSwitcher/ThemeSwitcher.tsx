40a21ac911e1f544b9e11636b3f5944fc4bf6d49
maximobelets <maximobelets@gmail.com>
2026-02-05 22:36:15 +0500

    Update themeSwitcher

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Button } from '../Button';
 
 import s from './ThemeSwitcher.module.css';
 
 interface ThemeSwitcherProps {
-	children?: React.ReactNode;
-	type?: 'submit' | 'reset' | 'button';
-	onClick?: () => void;
-	variant?: 'standard' | 'outline' | 'cancel'
+	// add later
 }
 
 export const ThemeSwitcher = () => {
+	const [theme, setTheme] = useState('light');
+
+	const changeTheme = () => setTheme(prevState => prevState === 'light' ? 'dark' : 'light');
+
 	return (
-		<div>
-			ThemeSwitcher
-		</div>
+		<Button onClick={changeTheme}>
+			Theme is {theme}
+		</Button>
 	)
 };
