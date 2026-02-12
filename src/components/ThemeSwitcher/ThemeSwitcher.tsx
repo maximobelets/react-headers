98d65125aad6ce090ceaf9944f20bda09a8c3582
maximobelets <maximobelets@gmail.com>
2026-02-12 23:35:38 +0500

    Update ThemeSwitcher

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 
 import SunIcon from '../../assets/icons/sun-icon.svg?react';
+import HalfMoonIcon from '../../assets/icons/half-moon-icon.svg?react';
 
 import s from './ThemeSwitcher.module.css';
 
 export const ThemeSwitcher = () => {
-	const [on, setOn] = useState(false);
+	const [isDarkTheme, setIsDarkTheme] = useState(false);
 
 	return (
 		<div
-			className={`${s.root} ${on && s.on}`}
-			onClick={() => setOn(!on)}
+			className={`${s.root} ${isDarkTheme && s.on}`}
+			onClick={() => setIsDarkTheme(!isDarkTheme)}
 		>
-			<div className={`${s.thumb} ${on && s.thumbAnimation}`} />
-			<SunIcon />
+			<div className={`${s.thumb} ${isDarkTheme && s.thumbAnimation}`}>
+				{isDarkTheme ? (
+					<HalfMoonIcon className={s.icon} />
+				): (
+					<SunIcon className={s.icon} />
+				)}
+			</div>
 		</div>
 	)
 };
