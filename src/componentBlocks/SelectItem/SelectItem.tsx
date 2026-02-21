3eba83f298776d703a6e809d535ca205b2cb06eb
maximobelets <maximobelets@gmail.com>
2026-02-21 22:08:01 +0500

    Update SelectItem

--- src/componentBlocks/SelectItem/SelectItem.tsx
+++ src/componentBlocks/SelectItem/SelectItem.tsx
@@ -5,14 +5,16 @@ import crossIcon from '../../components/Nav/assets/cross.svg';
 import s from './SelectItem.module.css';
 
 export const SelectItem = ({ name, onClick }: ISelectItem) => {
+	const handleClick = () => onClick(name);
+
 	return (
-		<span className={s.root} key={name}>
+		<span className={s.root}>
 			{name}
             <img
 				src={crossIcon}
 				alt={'Icon for open delete select item'}
 				className={s.icon}
-				onClick={() => onClick(name)}
+				onClick={handleClick}
 			/>
 		</span>
 	)
